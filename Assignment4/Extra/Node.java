ff316b78b6c8ea05e8a88caae13e0a1345164f41
David Li <FriendlyUser@users.noreply.github.com>
2016-05-20 20:54:22 -0700

    Add files via upload

---
+++ Assignment4/Extra/Node.java
@@ -0,0 +1,18 @@
+/*  David Li V00818631
+*	Node.java
+*	This file contains the Nodes used in the linked list
+*   Each Node has a string and a link to the next node
+*/
+public class Node {
+	Node next;
+	String data;
+	
+	public Node(String data) {
+		this.next = null;
+		this.data = data;
+	}
+	public Node(String data, Node next) {
+		this.data = data;
+		this.next = next;
+	}
+}
